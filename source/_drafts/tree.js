c1a3484f1e3018b1df8d7ee2ee606274ca22eca9
王茂己 <wangmaoji@wangmaojideMacBook-Pro.local>
2022-04-29 01:24:17 +0800

    code: preOrder、InOrder（unSolve)

--- source/_drafts/tree.js
+++ source/_drafts/tree.js
@@ -1,18 +1,113 @@
-const Queue = () => {
-  let list = []
-  let add = value => list.push(value)
+// const Queue = () => {
+//   let list = []
+//   let add = value => list.push(value)
 
-  return {
-    add,
-    list,
-  }
-}
+//   return {
+//     add,
+//     list,
+//   }
+// }
 
-const myQueue = Queue()
+// const myQueue = Queue()
 
-const BFTT = () => {
-  myQueue.add()
-  for (let i = 0; i < myQueue.list.length; i++) {
-    myQueue.add()
-  }
-}
+// const BFTT = () => {
+//   myQueue.add()
+//   for (let i = 0; i < myQueue.list.length; i++) {
+//     myQueue.add()
+//   }
+// }
+[2, 7, 2, 10, 6, 5, 11, 5, 9, 4]
+
+const tree = {
+  value: 2,
+  children: [
+    {
+      value: 7,
+      children: [
+        {
+          value: 2,
+          children: null,
+        },
+        {
+          value: 10,
+          children: null,
+        },
+        {
+          value: 6,
+          children: [
+            {
+              value: 5,
+              children: null,
+            },
+            {
+              value: 11,
+              children: null,
+            },
+          ],
+        },
+      ],
+    },
+    {
+      value: 5,
+      children: [
+        {
+          value: 9,
+          children: [
+            {
+              value: 4,
+              children: null,
+            }
+          ],
+        },
+      ],
+    },
+  ],
+};
+
+// const preOrder = (root) => {
+//   let queue = []
+
+//   const traversal = node => {
+//     queue.push(node.value)
+//     if (node.children !== null) {
+//       for (let i = 0; i < node.children.length; i++) {
+//         traversal(node.children[i]);
+//       }
+//     }
+//   }
+
+//   traversal(root);
+
+//   return queue
+// }
+
+// let result = preOrder(tree)
+// console.log(result)
+
+const InOrder = (root) => {
+  let queue = [];
+
+  const traversal = (node) => {
+    if (node.children !== null) {
+      for (let i = 0; i < node.children.length; i++) {
+        traversal(node.children[i]);
+        if (i === 0) {
+          queue.concat([node.children[i], node.value]); 
+        }
+        if (i === node.children.length - 1) queue.push(node.children[i])
+      }
+    } else {
+      queue.push(node.value);
+    }
+  };
+
+  traversal(root);
+
+  return queue;
+};
+
+let result = InOrder(tree);
+console.log(result);
+
+
+
--- source/_drafts/tree.md
+++ source/_drafts/tree.md
@@ -24,3 +24,24 @@ BFTT: 2, 7, 5, 2, 10, 6, 9, 2, 11, 4
 
 use queue
 
+### Depth-First Tree Traversal（深度優先）
+
+
+Pre-Order 
+
+處理根節點後，透過遞迴的方式處理所有的子樹(subtree)，直到遍歷所有的節點。
+
+原則：root, left, right
+
+[
+  2,  7, 2, 10, 6,
+  5, 11, 5,  9, 4
+]
+
+In-Order 
+
+原則：left, root, right
+
+[
+  2, 7, 10, 5, 6, 11, 2, 4, 9, 5
+]
